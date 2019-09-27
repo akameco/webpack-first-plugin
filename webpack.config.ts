d130fdeabb732a2ee7a5bb37afee7818e5f5dd91
akameco <akameco.t@gmail.com>
2019-09-27 16:09:37 +0900

    update

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,19 +2,42 @@ import { Compiler, Plugin, Configuration } from "webpack";
 
 class HelloWorldPlugin implements Plugin {
   apply(compiler: Compiler) {
-    compiler.hooks.done.tap("Hello World Plugin", (
-      stats /* stats is passed as argument when done hook is tapped.  */
-    ) => {
-      console.log("Hello World!");
-      console.log(stats);
+    compiler.hooks.compilation.tap("HelloCompilationPlugin", compilation => {
+      compilation.hooks.optimize.tap("HelloCompilationPlugin", () => {
+        console.log("Assets are being optimized.");
+      });
+    });
+  }
+}
+
+class FileListPlugin implements Plugin {
+  apply(compiler: Compiler) {
+    // emit is asynchronous hook, tapping into it using tapAsync, you can use tapPromise/tap(synchronous) as well
+    compiler.hooks.emit.tapAsync("FileListPlugin", (compilation, callback) => {
+      // Create a header string for the generated file:
+      let filelist = "In this build:\n\n";
+      // Loop through all compiled assets,
+      // adding a new line item for each filename.
+      for (var filename in compilation.assets) {
+        filelist += "- " + filename + "\n";
+      }
+      // Insert this list into the webpack build as a new file asset:
+      compilation.assets["filelist.md"] = {
+        source: function() {
+          return filelist;
+        },
+        size: function() {
+          return filelist.length;
+        }
+      };
+      callback();
     });
   }
 }
 
 const config: Configuration = {
   mode: "development",
-  // @ts-ignore
-  plugins: [new HelloWorldPlugin({ hello: "hello" })]
+  plugins: [new FileListPlugin()]
 };
 
-module.exports = config;
+export default config;
